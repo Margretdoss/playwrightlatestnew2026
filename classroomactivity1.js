e47caf2184924908733c51f1316cd5b5c93c34ed
Margretdoss <51856068+Margretdoss@users.noreply.github.com>
2026-06-26 08:26:53 +0530

    Add files via upload

---
+++ classroomactivity1.js
@@ -0,0 +1,32 @@
+/*Assignment Details:
+Declare variables using `var` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable.
+Assignment Requirements:
+Create the following variables using let (not using var) and check their typeOf
+a) firstName
+b) companyName
+c) mobileNumber
+d) isAutomation
+e) hasPlaywright (do not assign)
+Print and confirm the values and data types
+Hints to Solve:
+Focus on initializing variables with different values, including a string, number, boolean, and leave one
+variable undefined to practice with `typeof`.
+Expected Outcome:
+Upon completion, you should be able to:
+- Grasp the concepts of different data types in JavaScript*/
+
+
+
+let firstName = "John";
+let companyName = "ABC Corp";
+let mobileNumber = 1234567890;
+let isAutomation = true;
+let containplaywright ;
+let hasPlaywright = "NULL";
+
+console.log(mobileNumber);
+console.log(typeof mobileNumber );
+
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright );
